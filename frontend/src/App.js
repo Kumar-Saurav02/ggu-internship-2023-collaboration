07e76ad222afdd32daf8db3686fec016802de6b9
Abhishek kumar <abhishekkr.singh944@gmail.com>
2023-05-17 11:20:38 +0530

    student scholarship

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/RegisterLogin/Register";
 import Login from "./components/RegisterLogin/Login";
+import SScholarship from "./components/studentScholarship/studentScholarship";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path = "/sScholarship" element={<SScholarship/>}/>
       </Routes>
     </Router>
   );
