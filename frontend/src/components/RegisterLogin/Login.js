962f167ae33d2d1649dc779a04d15d4657dff697
golu <krsaurav199@gmail.com>
2023-05-19 00:24:40 +0530

    login page css added

--- frontend/src/components/RegisterLogin/Login.js
+++ frontend/src/components/RegisterLogin/Login.js
@@ -1,5 +1,9 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./Login.css";
+import formfill from "../../Images/form_fill.jpg";
+import ggulogo from "../../Images/logo_pic.jpg";
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+
 
 const Login = () => {
   const categoriesOfUser = ["Student", "Teacher", "HOD"];
@@ -29,71 +33,140 @@ const Login = () => {
   const [passwordTeacher, setPasswordTeacher] = useState("");
 
   return (
-    <Fragment>
-      <div>
-        <div className="heading">
-          {!loginStudent && !loginTeacher && <h2>Login</h2>}
-          {loginStudent && !loginTeacher && <h2>Login as Student</h2>}
-          {!loginStudent && loginTeacher && <h2>Login as Teacher</h2>}
+
+    <div className="loginBox">
+        <div className="lefthalf">
+            <img
+              class="loginImg"
+              src={formfill}
+              alt="student form fill"
+            />
         </div>
-        <div className="toggleForLogin">
-          <select onChange={(e) => setTypeOfUser(e.target.value)}>
-            <option value={typesOfUser}>User</option>
-            {categoriesOfUser.map((user) => (
-              <option key={user} value={user}>
-                {user}
-              </option>
-            ))}
-          </select>
-        </div>
-        {loginStudent && (
-          <div>
-            <div>
-              <input
-                type="text"
-                placeholder="Enrollment Number"
-                required
-                value={enrollmentNo}
-                onChange={(e) => setEnrollmentNo(e.target.value)}
-              />
+        <div className="righthalf">
+
+            <div class="welcome">
+              <div ><img class="ggulogo" src={ggulogo} alt="ggu logo" /></div>
+              <div>
+                <h1>Welcome to <br />Guru Ghasidas </h1>
+              </div>
             </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                required
-                value={passwordStudent}
-                onChange={(e) => setPasswordStudent(e.target.value)}
-              />
-            </div>
-            <button>Login</button>
+
+
+            <Fragment >
+              <div className="full ">
+                <div className="heading">
+                  {!loginStudent && !loginTeacher && <h2>Login</h2>}
+                  {loginStudent && !loginTeacher && <h2>Login as Student</h2>}
+                  {!loginStudent && loginTeacher && <h2>Login as Teacher</h2>}
+                </div>
+
+                <div className="toggleForLogin selct_user ">
+                  
+                  <select className="border" onChange={(e) => setTypeOfUser(e.target.value)}>
+                    <option  value={typesOfUser}>User</option>
+                    {categoriesOfUser.map((user) => (
+                      <option key={user} value={user}>
+                        {user}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className="placed">
+                  {!loginStudent && !loginTeacher && 
+                      <div>
+                      
+                        <div >
+                            <input className="border"
+                              type="text"
+                              placeholder="Enter ID"
+                              required
+                            />
+                        </div>
+                        <div >
+                            <input className="border"
+                              type="password"
+                              placeholder="Enter Password"
+                              required
+                            />
+                        </div>
+                          <div className="sign_in">
+                        <button className="signInbtn border hover">Login</button>
+                        <div className="regandfor">
+                            <button className="signInbtn border hover">New Register</button>
+                            <a className="forlin" href="google.com">Forgot Password🤔</a>
+                        </div>
+                    </div>
+                      </div>   
+                  }
+                </div>
+
+              {loginStudent && 
+              (
+                  <div>
+                    <div >
+                      <input className="border"
+                        type="text"
+                        placeholder="Enrollment Number"
+                        required
+                        value={enrollmentNo}
+                        onChange={(e) => setEnrollmentNo(e.target.value)}
+                      />
+                    </div>
+                    <div >
+                      <input className="border"
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={passwordStudent}
+                        onChange={(e) => setPasswordStudent(e.target.value)}
+                      />
+                    </div>
+                    <div className="sign_in">
+                        <button className="signInbtn border hover">Login</button>
+                        <div className="regandfor">
+                            <button className="signInbtn border hover">New Register</button>
+                            <a className="forlin" href="google.com">Forgot Password🤔</a>
+                        </div>
+                    </div>
+                  </div>
+                )}
+                {loginTeacher && (
+                  <div>
+                    <div >
+                      <input className="border"
+                        type="email"
+                        placeholder="Email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div >
+                      <input className="border"
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={passwordTeacher}
+                        onChange={(e) => setPasswordTeacher(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="sign_in">
+                        <button className="signInbtn border hover">Login</button>
+                        <div className="regandfor">
+                            <button className="signInbtn border hover">New Register</button>
+                            <a className="forlin" href="google.com">Forgot Password🤔</a>
+                        </div>
+                          
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Fragment>
+            
           </div>
-        )}
-        {loginTeacher && (
-          <div>
-            <div>
-              <input
-                type="email"
-                placeholder="Email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                required
-                value={passwordTeacher}
-                onChange={(e) => setPasswordTeacher(e.target.value)}
-              />
-            </div>
-            <button>Login</button>
-          </div>
-        )}
-      </div>
-    </Fragment>
+    </div>
   );
 };
 
